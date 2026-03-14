5503d98299a2fa4ac4111b1cb26b9ed02d3af91e
Your Name <you@example.com>
2026-03-14 17:57:41 -0300

    API teste

--- src/app/features/users/services/user.service.ts
+++ src/app/features/users/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
